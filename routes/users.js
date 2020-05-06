c25112178ef0318cb8b93eb2cf88c912dd1f37d7
Giovanni Flores <geflores5@gmail.com>
2020-05-06 16:33:50 -0700

    address timeout error

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ router.route('/').get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err))
-});
+})
 
 router.route('/add').post((req, res) => {
   const username = req.body.username
--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(cors())
 app.use(express.json())
 
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect(uri, { 
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: false
+}).catch(err => console.log(err))
 
 const connection = mongoose.connection
 connection.once('open', () => {
