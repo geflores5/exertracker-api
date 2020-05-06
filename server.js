bb343d7ca47458e1d2420a2af7c81dff1385a71c
Giovanni Flores <geflores5@gmail.com>
2020-05-06 15:16:57 -0700

    cleanup

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const exercisesRouter = require('./routes/exercises')
 const usersRouter = require('./routes/users')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
 const connection = mongoose.connection
 connection.once('open', () => {
